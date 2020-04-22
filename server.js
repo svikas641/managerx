6483f5db3035bae7b5b5dbee2b03c51acb808261
Vikas_Kumar <svikas641@gmail.com>
2020-04-22 14:06:06 +0530

    minor modifications

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express  = require('express');
+//const cors = require('cors')
 const connectDB = require('./config/db');
 
 const app = express();
 
 //connect Database
 connectDB();
+
+// Middlewares
+//app.use(cors());
+app.use(express.json({extended: false}));
+
+// Define routes
+app.use('/api/users',require('./routes/api/user'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/feedback',require('./routes/api/feedback'));
+app.use('/api/lead',require('./routes/api/lead'));
+
 
 const PORT = process.env.PORT || 5000;
 
