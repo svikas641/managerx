331c07d06c70c862e5686d304d017c52980122e0
Vikas_Kumar <svikas641@gmail.com>
2020-05-14 16:31:25 +0530

    Added a separate file for LeadItem

---
+++ client/src/components/lead/LeadItem.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+
+const LeadItem = ({auth, lead: {_id, companyName,clientName,clientEmail,clientPhoneNumber,clientAddress,pincode,salesPerson,date},fullContent}) => (
+	<div>
+      <li className="list-group-item">{companyName} - Created on <Moment format='DD/MM/YYYY'>{date}</Moment> </li>
+      	  <li className='list-group-item'>Client Name : {clientName}</li>
+      	  <li className='list-group-item'>Client Email : {clientEmail}</li>
+      	  <li className='list-group-item'>Client Phone Number : {clientPhoneNumber}</li>
+      	  <li className='list-group-item'>Client Address : {clientAddress}</li>
+      	  <li className='list-group-item'>Pincode : {pincode}</li>
+      	  <li className='list-group-item'>Lead created by : {salesPerson}</li>
+    </div>   
+);
+
+LeadItem.defaultProps = {
+	fullContent: false
+}
+
+LeadItem.propTypes = {
+	lead: PropTypes.object.isRequired,
+	auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state =>({
+	auth:state.auth
+})
+
+export default connect(mapStateToProps, {})(LeadItem);
