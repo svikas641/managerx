66202b7a9e946993f467b4be2d830bd1fd3756ed
Vikas_Kumar <svikas641@gmail.com>
2020-05-23 11:32:40 +0530

    prettier

--- routes/api/distance.js
+++ routes/api/distance.js
@@ -1,19 +1,19 @@
-const express =  require('express');
+const express = require("express");
 const router = express.Router();
-const DistanceController = require('../../controller/DistanceController');
-const mode = ['bycicling', 'walking', 'driving'];
+const DistanceController = require("../../controller/DistanceController");
+const mode = ["bycicling", "walking", "driving"];
 
-router.post('/location', (req, res) => {
-		const isValid = (typeof req.body.origin === 'string');
-		if (!isValid) res.status(500).send('Server Error');
-		if (mode[req.body.mode] !== undefined) req.body.mode = 'driving';
-		 DistanceController(req.body.origin, req.body.destination, req.body.mode)
-		 .then(result => {
+router.post("/location", (req, res) => {
+	const isValid = typeof req.body.origin === "string";
+	if (!isValid) res.status(500).send("Server Error");
+	if (mode[req.body.mode] !== undefined) req.body.mode = "driving";
+	DistanceController(req.body.origin, req.body.destination, req.body.mode)
+		.then((result) => {
 			res.json(result);
-		}).catch(errResult => {
-			res.status(500).send('Main Server Error');
+		})
+		.catch((errResult) => {
+			res.status(500).send("Main Server Error");
 		});
-	});
-
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express  = require('express');
-const connectDB = require('./config/db');
-const path = require('path')
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -10,24 +10,21 @@ connectDB();
 app.use(express.json());
 
 // Define routes
-app.use('/api/users',require('./routes/api/user'));
-app.use('/api/auth',require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/lead',require('./routes/api/lead'));
-app.use('/api/map',require('./routes/api/distance'));
-
+app.use("/api/users", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/lead", require("./routes/api/lead"));
+app.use("/api/map", require("./routes/api/distance"));
 
 //Serve Static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
 }
-
-
 
 const PORT = process.env.PORT || 5000;
 
